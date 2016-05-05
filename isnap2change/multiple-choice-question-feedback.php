59a7fdef8abdc80607b9e5a788c805435c5c0279
haokanga <haokanga@andrew.cmu.edu>
2016-05-05 21:27:53 +0930

    check whether the student passed this MCQ compared to threshold

--- isnap2change/multiple-choice-question-feedback.php
+++ isnap2change/multiple-choice-question-feedback.php
@@ -42,21 +42,39 @@
 					for(j = 0; j < options.length; j++){ ";
 						
 					if(!isset($answerArr[$i])){
-                        
-                        //SQL UPDATE STATEMENT
-						$update_stmt = "REPLACE INTO MCQ_Question_Record(StudentID, MCQID, Choice)
-                                     VALUES (?,?,?);";			
-                        $update_stmt = $conn->prepare($update_stmt);                            
-                        if(! $update_stmt -> execute(array($studentID, $MCQIDArr[$i], $answerArr[$i]))){
-                            echo "<script language=\"javascript\">  alert(\"Error occurred to submit your answer. Report this bug to reseachers.\"); </script>";
-                        }
 						echo "
 							if(options[j].value == ".$mcqGradeRes->CorrectChoice."){
 								contents[j].style.background=\"#00ff00\";
 							}
 							";
 							
-					} else {
+					} else {                        
+                            $threshold = count($MCQIDArr)*0.8;
+                            //SQL UPDATE STATEMENT
+                            $sql = $conn->prepare('SELECT COUNT(*) FROM MCQ_Question WHERE `MCQID` = BINARY :mcqid AND `CorrectChoice` = BINARY :correctchoice');
+                            $sql->bindParam(':mcqid', $MCQIDArr[$i]);
+                            $sql->bindParam(':correctchoice', $answerArr[$i]);
+                            $sql->execute();
+                            if ($sql->fetchColumn() >= $threshold) {
+                                //SQL UPDATE STATEMENT
+                                $update_stmt = "REPLACE INTO MCQ_Question_Record(StudentID, MCQID, Choice)
+                                             VALUES (?,?,?);";			
+                                $update_stmt = $conn->prepare($update_stmt);                            
+                                if(! $update_stmt -> execute(array($studentID, $MCQIDArr[$i], $answerArr[$i]))){
+                                    echo "<script language=\"javascript\">  alert(\"Error occurred to submit your answer. Report this bug to reseachers.\"); </script>";
+                                }
+                            } else{
+                                //DEMO CODE to check the if-else structure
+                                /*
+                                $update_stmt = "REPLACE INTO MCQ_Question_Record(StudentID, MCQID, Choice)
+                                             VALUES (?,?,?);";			
+                                $update_stmt = $conn->prepare($update_stmt);                            
+                                if(! $update_stmt -> execute(array($studentID, $MCQIDArr[$i], "WRONG"))){
+                                    echo "<script language=\"javascript\">  alert(\"Error occurred to submit your answer. Report this bug to reseachers.\"); </script>";
+                                }
+                                */
+                                
+                            }
 							
 							if($mcqGradeRes->CorrectChoice == $answerArr[$i]){
 							
