9d0b7f1435d4d0a2ff4db394c2aada3d962ee47c
haokanga <haokanga@andrew.cmu.edu>
2016-05-09 11:03:30 +0930

    session-timeout-test.php explict timeout

--- isnap2change/session-timeout-test.php
+++ isnap2change/session-timeout-test.php
@@ -1,4 +1,6 @@
 <?php
+    /**
+    session_set_cookie_params(5);
 	session_start();        
     if(isset($_SESSION['userid'])){
         $studentID = $_SESSION['userid'];
@@ -7,4 +9,20 @@
         echo "<script language=\"javascript\">  console.log(\"SESSION EXPIRED!\"); </script>";
         $studentID = 1;
     }
+    */
+    
+    session_start();
+    $idletime=5;//after n seconds the user gets logged out
+    if (isset($_SESSION['timestamp']) && time()-$_SESSION['timestamp']>$idletime){
+        session_destroy();
+        session_unset();
+        echo "<script language=\"javascript\">  console.log(\"SESSION EXPIRED!\"); </script>";
+        header("location: login.php");
+    }else{
+        $studentID = $_SESSION['userid'];
+        echo "<script language=\"javascript\">  console.log(\"This is DEBUG_MODE with SESSION studentID = ".$studentID.".\"); </script>";
+        $_SESSION['timestamp']=time();
+    }
+    //on session creation
+    $_SESSION['timestamp']=time();
 ?>
