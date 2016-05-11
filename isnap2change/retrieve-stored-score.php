d6aa5a2d7986ab6a6b7cab535d514f6b75693814
haokanga <haokanga@andrew.cmu.edu>
2016-05-11 19:13:16 +0930

    modularize retrieve-stored-score.php and upload-score.php

--- isnap2change/retrieve-stored-score.php
+++ isnap2change/retrieve-stored-score.php
@@ -1,63 +1,54 @@
 <?php 
     //if true, echo debug output in dev mode, else production mode
 	$DEBUG_MODE = true;
-    session_start();    
+    $NUM_OF_LEVEL[] = 5;
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    }   
 	require_once('connection.php');
     if ($_SERVER["REQUEST_METHOD"] == "GET") {        
         if(isset($_GET["command"]) && isset($_GET["gameid"])){
             $gameid = $_GET["gameid"];
-            if($_GET["command"] == "retrieve" && $gameid == 1){                
-                $NUM_OF_LEVEL = 5;
+            if($_GET["command"] == "retrieve" && $gameid == 1){
                 if(isset($_SESSION['userid'])){
                     $studentid = $_SESSION['userid'];
                     echo "<script language=\"javascript\">  console.log(\"This is DEBUG_MODE with SESSION studentid = ".$studentid.".\"); </script>";
-                    $conn = db_connect();                    
-                    for($i=1; $i<=$NUM_OF_LEVEL; $i++){
-                        $retrieveScorePreSql = "SELECT COUNT(*) FROM Game_Record WHERE `GameID` = ? AND `StudentID` = ? AND `Level` = ?";
-                        $retrieveScorePreQuery = $conn->prepare($retrieveScorePreSql);
-                        $retrieveScorePreQuery->execute(array($gameid, $studentid, $i));
-                        if ($retrieveScorePreQuery->fetchColumn() > 0) {
-                            $retrieveScoreSql = "SELECT GameID,StudentID,`Level`,Score FROM Game_Record WHERE `GameID` = ? AND `StudentID` = ? AND `Level` = ?";
-                            $retrieveScoreQuery = $conn->prepare($retrieveScoreSql);
-                            $retrieveScoreQuery->execute(array($gameid, $studentid, $i));
-                            $retrieveScoreResult = $retrieveScoreQuery->fetch(PDO::FETCH_OBJ);
-                            echo "<script language=\"javascript\">  console.log(\"[SUCCESS] Game Record Found. gameid: $gameid  studentid: $studentid level: $i score:".$retrieveScoreResult->Score."\"); </script>";
-                            $scoreArray[] = $retrieveScoreResult->Score;
-                        }
-                        else {
-                             echo "<script language=\"javascript\">  console.log(\"[INFO] No Game Record Found. gameid: $gameid  studentid: $studentid level: $i score:null(set as 0)\"); </script>";
-                             $scoreArray[] = 0;
-                        } 
-                    }                    
-                    db_close($conn);
+                    $scoreArray = retrieve_data(); 
                 }else{
                     echo "<script language=\"javascript\">  console.log(\"You have not logged in.\"); </script>";
                     if($DEBUG_MODE) {
                         $studentid = 1;
                         echo "<script language=\"javascript\">  console.log(\"This is DEBUG_MODE with TEST studentid = ".$studentid.".\"); </script>";
-                        $conn = db_connect();
-                        for($i=1; $i<=$NUM_OF_LEVEL; $i++){
-                            $retrieveScorePreSql = "SELECT COUNT(*) FROM Game_Record WHERE `GameID` = ? AND `StudentID` = ? AND `Level` = ?";
-                            $retrieveScorePreQuery = $conn->prepare($retrieveScorePreSql);
-                            $retrieveScorePreQuery->execute(array($gameid, $studentid, $i));
-                            if ($retrieveScorePreQuery->fetchColumn() > 0) {
-                                $retrieveScoreSql = "SELECT GameID,StudentID,`Level`,Score FROM Game_Record WHERE `GameID` = ? AND `StudentID` = ? AND `Level` = ?";
-                                $retrieveScoreQuery = $conn->prepare($retrieveScoreSql);
-                                $retrieveScoreQuery->execute(array($gameid, $studentid, $i));
-                                $retrieveScoreResult = $retrieveScoreQuery->fetch(PDO::FETCH_OBJ);
-                                echo "<script language=\"javascript\">  console.log(\"[SUCCESS] Game Record Found. gameid: $gameid  studentid: $studentid level: $i score:".$retrieveScoreResult->Score."\"); </script>";
-                                $scoreArray[] = $retrieveScoreResult->Score;
-                            }
-                            else {
-                                 echo "<script language=\"javascript\">  console.log(\"[INFO] No Game Record Found. gameid: $gameid  studentid: $studentid level: $i score:null(set as 0)\"); </script>";
-                                 $scoreArray[] = 0;
-                            } 
-                        }                        
-                        db_close($conn);
+                        $scoreArray = retrieve_data(); 
                     }                
                 }
-                echo "score array:".join(',', $scoreArray);;
+                echo "score array:".join(',', $scoreArray);
             }
         }
+    }
+    function retrieve_data()
+    {
+        global $NUM_OF_LEVEL, $gameid, $studentid;
+        $conn = db_connect();                    
+        for($i=1; $i<=$NUM_OF_LEVEL[0]; $i++){
+            $retrieveScorePreSql = "SELECT COUNT(*) FROM Game_Record WHERE `GameID` = ? AND `StudentID` = ? AND `Level` = ?";
+            $retrieveScorePreQuery = $conn->prepare($retrieveScorePreSql);
+            $retrieveScorePreQuery->execute(array($gameid, $studentid, $i));
+            if ($retrieveScorePreQuery->fetchColumn() > 0) {
+                $retrieveScoreSql = "SELECT GameID,StudentID,`Level`,Score FROM Game_Record WHERE `GameID` = ? AND `StudentID` = ? AND `Level` = ?";
+                $retrieveScoreQuery = $conn->prepare($retrieveScoreSql);
+                $retrieveScoreQuery->execute(array($gameid, $studentid, $i));
+                $retrieveScoreResult = $retrieveScoreQuery->fetch(PDO::FETCH_OBJ);
+                echo "<script language=\"javascript\">  console.log(\"[SUCCESS] Game Record Found. gameid: $gameid  studentid: $studentid level: $i score:".$retrieveScoreResult->Score."\"); </script>";
+                $scoreArray[] = $retrieveScoreResult->Score;
+            }
+            else {
+                 echo "<script language=\"javascript\">  console.log(\"[INFO] No Game Record Found. gameid: $gameid  studentid: $studentid level: $i score:null(set to 0)\"); </script>";
+                 $scoreArray[] = 0;
+            }
+        }
+        db_close($conn);
+        return $scoreArray;
     }
 ?>
--- isnap2change/upload-score.php
+++ isnap2change/upload-score.php
@@ -1,8 +1,12 @@
 <?php 
     //if true, echo debug output in dev mode, else production mode
 	$DEBUG_MODE = true;
-    session_start();    
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    }    
 	require_once('connection.php');
+    require_once('retrieve-stored-score.php');
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
         if(isset($_GET["score"]) && isset($_GET["gameid"])){
             $score = $_GET["score"];
@@ -10,38 +14,38 @@
             if(isset($_SESSION['userid'])){
                 $studentid = $_SESSION['userid'];
                 echo "<script language=\"javascript\">  console.log(\"This is DEBUG_MODE with SESSION studentid = ".$studentid.".\"); </script>";
-                $conn = db_connect();
-                $update_stmt = "INSERT INTO Game_Record(GameID,StudentID,`Level`,Score)
-                             VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE Score = ?;";			
-                $update_stmt = $conn->prepare($update_stmt);
-                for($i=1; $i<=count($score); $i++){               
-                    if(! $update_stmt -> execute(array($gameid, $studentid, $i, $score[$i-1], $score[$i-1]))){
-                        echo "<script language=\"javascript\">  alert(\"Error occurred to submit game score. Report this bug to reseachers.\"); </script>";
-                    } else{            
-                        echo "<script language=\"javascript\">  console.log(\"Game Record Submitted. gameid: $gameid  studentid: $studentid\"); </script>";
-                    }
-                }
-                db_close($conn);
+                $highscore = retrieve_data();
+                upload_score();
             }else{
                 echo "<script language=\"javascript\">  console.log(\"You have not logged in.\"); </script>";
                 if($DEBUG_MODE) {
                     $studentid = 1;
                     echo "<script language=\"javascript\">  console.log(\"This is DEBUG_MODE with TEST studentid = ".$studentid.".\"); </script>";
-                    $conn = db_connect();
-                    $update_stmt = "INSERT INTO Game_Record(GameID,StudentID,`Level`,Score)
-                             VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE Score = ?;";			
-                    $update_stmt = $conn->prepare($update_stmt);
-                    for($i=1; $i<=count($score); $i++){               
-                        if(! $update_stmt -> execute(array($gameid, $studentid, $i, $score[$i-1], $score[$i-1]))){
-                            echo "<script language=\"javascript\">  alert(\"Error occurred to submit game score. Report this bug to reseachers.\"); </script>";
-                        } else{            
-                            echo "<script language=\"javascript\">  console.log(\"Game Record Submitted. gameid: $gameid  studentid: $studentid\"); </script>";
-                        }
-                    }
-                    db_close($conn);
+                    $highscore = retrieve_data();                    upload_score();
                 }
             }            
         }
+    }
+    function upload_score()
+    {
+        global $gameid, $studentid, $score, $highscore;
+        $conn = db_connect();
+        $update_stmt = "INSERT INTO Game_Record(GameID,StudentID,`Level`,Score)
+                     VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE Score = ?;";			
+        $update_stmt = $conn->prepare($update_stmt);
+        for($level=1; $level<=count($score); $level++){
+            if($score[$level-1]>$highscore[$level-1]){
+                if(! $update_stmt -> execute(array($gameid, $studentid, $level, $score[$level-1], $score[$level-1]))){
+                    echo "<script language=\"javascript\">  alert(\"Error occurred to submit game score. Report this bug to reseachers.\"); </script>";
+                } else{            
+                    echo "<script language=\"javascript\">  console.log(\"Game Record Submitted. gameid: $gameid  studentid: $studentid\"); </script>";
+                }
+            }
+            else{
+                echo "<script language=\"javascript\">  console.log(\"Score does not exceed highscore. highscore: ".$highscore[$level-1]."  score: ".$score[$level-1]."\"); </script>";
+            }
+        }
+        db_close($conn);
     }
 ?>
 <html>
