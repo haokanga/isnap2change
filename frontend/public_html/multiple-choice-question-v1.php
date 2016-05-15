ddd0e1e814e6855cfd113ed08dad0aed9d6f5a01
wwjsky <weijunw@andrew.cmu.edu>
2016-05-15 15:15:18 +0930

    backend logic for quiz

--- frontend/public_html/multiple-choice-question-v1.php
+++ frontend/public_html/multiple-choice-question-v1.php
@@ -25,7 +25,7 @@
 	
 	$conn = db_connect();
 	
-	$mcqSql = "SELECT MCQID, Question, Content 
+	$mcqSql = "SELECT MCQID, Question, Content
 			   FROM   MCQ_Section NATURAL JOIN MCQ_Question
 								  NATURAL JOIN `Option`
 			   WHERE  QuizID = ?
@@ -46,13 +46,13 @@
 
 <html>
     <head>
-        <title>QUIZ</title>
+        <title>Quiz</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href='https://fonts.googleapis.com/css?family=Raleway:400italic|Open+Sans' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/feedback.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" />
-        <link rel="stylesheet" href="css/ver2.css" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400|Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
         <script type="text/javascript" src="js/jquery-1.12.3.js"></script>
     </head>
     <body>
@@ -82,26 +82,78 @@
                 });
 
             });
-        </script>
+			
+			function parseScript(strcode) {
+				var scripts = new Array();         // Array which will store the script's code
+  
+				// Strip out tags
+				while(strcode.indexOf("<script") > -1 || strcode.indexOf("</script") > -1) {
+					var s = strcode.indexOf("<script");
+					var s_e = strcode.indexOf(">", s);
+					var e = strcode.indexOf("</script", s);
+					var e_e = strcode.indexOf(">", e);
+    
+					// Add to scripts array
+					scripts.push(strcode.substring(s_e+1, e));
+					// Strip from strcode
+					strcode = strcode.substring(0, s) + strcode.substring(e_e+1);
+			  }
+			  
+			  // Loop through every script collected and eval it
+			  for(var i=0; i<scripts.length; i++) {
+				try {
+				  eval(scripts[i]);
+				}
+				catch(ex) {
+				  // do what you want here when a script fails
+				}
+			  }
+			}
+			
+			function submitQuiz()
+			{
+				var MCQIDArr = document.getElementById("hiddenMCQIDArray").value.split(',');
+				var answerArr = new Array(MCQIDArr.length);	
+					
+				$(".options").each(function(i) {
+					$(this).find(".btn").each(function() {
+						if($(this).hasClass("active")){
+							answerArr[i]=$(this).val();
+						}
+					});
+				});
+				
+				var xmlhttp = new XMLHttpRequest();
+				
+				xmlhttp.onreadystatechange = function() {
+					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+						parseScript(xmlhttp.responseText);
+					} else {
+						
+					}
+				};
+				
+				xmlhttp.open("POST", "multiple-choice-question-feedback-v1.php", true);
+				xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+				xmlhttp.send("MCQIDArr="+JSON.stringify(MCQIDArr)+"&answerArr="+JSON.stringify(answerArr)+"&quizid="+<?php echo $quizid;?>);
 
-        <div class="row" style="position: fixed;">              
-            <div class="col-md-2 sidebar bar">
-                <img src="css/l2.jpg" alt="" class="img-circle" style="height: 70px; display: block;
-                     margin: 0 auto; padding-top:15px;">
-                <ul class="list-group lg" >
-                    <li class="list-group-item" style="margin-top:25px;">
-                        <input type="radio" name="radio1" id="radio1" />
-                        <label for="radio1">INFORMATION
-                        </label>
-                    </li>
-                    <li class="list-group-item" style="margin-top:10px;">
-                        <input type="radio" name="radio1" id="radio2" />
-                        <label for="radio2">QUESTION 1
-                        </label>
-                    </li>
-                    
-                </ul>
+			}
+			
+			
+        </script>
+		
+		<header class="navbar navbar-static-top bs-docs-nav">
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                </button>
+                <a class="navbar-brand" href="#">QUIZ</a>
+
             </div>
+
+        </header>
+		
+		<div class="content"> 
 
 		<?php for($i=0; $i<count($rows); $i++) {
 			
@@ -123,14 +175,15 @@
 		<?php
 			} $lastMCQID = $currentMCQID;?>
 								
-								<button type="button" class="btn btn-default btn-lg btn-block" name="<?php echo $rows[$i]->MCQID;?>" value="<?php echo $rows[$i]->Content;?>"><?php echo $rows[$i]->Content;?></button>
+								<button type="button" class="btn btn-default btn-lg btn-block" name="<?php echo $rows[$i]->MCQID;?>" value="<?php echo $rows[$i]->Content;?>">
+								<input type="radio"/><label><?php echo $rows[$i]->Content;?></label></button>
 			
 		<?php
 			  if(($i+1)==sizeof($rows)){ ?>
 							</div>
 							<br>
 							<div class="last">
-								<a class="btn btn-default" href="#" role="button" style="padding-top:8px;"><span class="glyphicon glyphicon-chevron-left"></span></a>
+								<a class="btn btn-default" role="button" style="padding-top:8px;"><span class="glyphicon glyphicon-chevron-left"></span></a>
 							</div>
 						</div>
 					</div>
@@ -157,8 +210,10 @@
 			  }
 		} ?>
             
-            <div class="col-md-1"></div>
+			<button type="button" onclick="return submitQuiz();" class="btn btn-success">SUBMIT</button>    
             <input type="hidden" id="hiddenIndex" value="1">
+			<input type=hidden id="hiddenMCQIDArray" value="<?php echo substr($MCQIDArray, 0, strlen($MCQIDArray)-1); ?>">
+		</div>
         </div>
     </body>
 </html>
