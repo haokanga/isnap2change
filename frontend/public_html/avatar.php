f74bb1ff65d7adb97fee79d3f68113514e6fe059
haokanga <haokanga@andrew.cmu.edu>
2016-05-17 13:29:39 +0930

    trivial

--- frontend/public_html/avatar.php
+++ frontend/public_html/avatar.php
@@ -1,38 +1,40 @@
 <?php
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	session_start();
+	//$studentid = 1;
+	$_SESSION["studentid"] = 1;
 
 ?>
 
 <html>
 <head>
 <script>
-function startWeeklyTask(){
-	document.getElementById("quiz").submit();	
+function startWeeklyTask1(){
+	
+	document.getElementById("weeklytask1").submit();
+	
 }
+function startWeeklyTask3(){
+	
+	document.getElementById("weeklytask3").submit();
+	
+}
+
 </script>
 </head>
 <body>
+<!--
+<div id="a" align="center">
+<form id="weeklytask1" action=weekly-task.php method=post>
+<button type=button onclick="startWeeklyTask1()"> Week 1 </button>
+<input  type=hidden name="week" value="1"></input>
+</form>
+<form id="weeklytask3" action=weekly-task.php method=post>
+<button type=button onclick="startWeeklyTask3()"> Week 3 </button>
+<input  type=hidden name="week" value="3"></input>
+</form>
+</div>
+-->
 <div align="center">
 	<button type=button onclick="startWeeklyTask()"> Week 1 </button>
 	<button type=button onclick="startWeeklyTask()"> Week 2 </button>
@@ -48,7 +50,12 @@ function startWeeklyTask(){
 <br>
 <div align="center">
 	<button type=button> Check Progress </button>
+    <form id="scoreboard" action=scoreboard.php method=post>
 	<button type=button> Check Scoreboard </button>
+    </form>
+</form>
 </div>
 </body>
 </html>
+
+
--- isnap2change/avatar.php
+++ isnap2change/avatar.php
@@ -23,6 +23,7 @@ function startWeeklyTask3(){
 </script>
 </head>
 <body>
+<!--
 <div id="a" align="center">
 <form id="weeklytask1" action=weekly-task.php method=post>
 <button type=button onclick="startWeeklyTask1()"> Week 1 </button>
@@ -31,6 +32,27 @@ function startWeeklyTask3(){
 <form id="weeklytask3" action=weekly-task.php method=post>
 <button type=button onclick="startWeeklyTask3()"> Week 3 </button>
 <input  type=hidden name="week" value="3"></input>
+</form>
+</div>
+-->
+<div align="center">
+	<button type=button onclick="startWeeklyTask()"> Week 1 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 2 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 3 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 4 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 5 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 6 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 7 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 8 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 9 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 10 </button>
+</div>
+<br>
+<div align="center">
+	<button type=button> Check Progress </button>
+    <form id="scoreboard" action=scoreboard.php method=post>
+	<button type=button> Check Scoreboard </button>
+    </form>
 </form>
 </div>
 </body>
