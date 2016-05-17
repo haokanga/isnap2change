343a8c62636a2979671d5d7f501ab9b5bb92b7a8
wwjsky <weijunw@andrew.cmu.edu>
2016-05-17 13:10:50 +0930

    avatar.php

---
+++ frontend/public_html/avatar.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+<html>
+<head>
+<script>
+function startWeeklyTask(){
+	document.getElementById("quiz").submit();	
+}
+</script>
+</head>
+<body>
+<div align="center">
+	<button type=button onclick="startWeeklyTask()"> Week 1 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 2 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 3 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 4 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 5 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 6 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 7 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 8 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 9 </button>
+	<button type=button onclick="startWeeklyTask()"> Week 10 </button>
+</div>
+<br>
+<div align="center">
+	<button type=button> Check Progress </button>
+	<button type=button> Check Scoreboard </button>
+</div>
+</body>
+</html>
