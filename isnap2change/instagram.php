7a164e9d105d97242edd298d0338a22ef689b114
haokanga <haokanga@andrew.cmu.edu>
2016-05-18 14:57:38 +0930

    instagram (not work because of permission issues)

---
+++ isnap2change/instagram.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<script type="text/javascript" src="js/instafeed.min.js"></script>
+</head>
+<body>
+    <div id="instafeed"></div>
+    <script type="text/javascript">
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: '8f72a8bd53ca4c7881724feda7959875',
+        accessToken: '3248827072.d443c17.f6e3c1b366324c03907c053d31f71fbd'
+    });
+    feed.run();
+    </script>
+    </body>
+</html>
