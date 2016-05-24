a8da28f270bd70dd79064e44fba4f05c7e977db1
haokanga <haokanga@andrew.cmu.edu>
2016-05-24 19:24:15 +0930

    [Fixed Bug] Unable to disable left/right button at week 1/10

--- avatar/js/index.js
+++ avatar/js/index.js
@@ -1,43 +1,37 @@
 $('#nav-right').on('click', function() {
-
+    var MAX_WEEK = 10;
 	var index = $("#hiddenIndex").val();
 
 	index++;
 
-	if(index == 10) {
+	if(index > MAX_WEEK) {
 		$('#nav-right').attr("disabled","disabled");
 	} else {
 		$('#nav-left').removeAttr("disabled");
-		$('#nav-right').removeAttr("disabled");
+		$('#nav-right').removeAttr("disabled");        
+        $("#hiddenIndex").val(index);	
+        var pos = 3 + index * 9;
+        $('.runner').css("left", pos+"%");
+        $('.runner').addClass('horizTranslate');
 	}
-
-	$("#hiddenIndex").val(index);	
-
-	var pos = 3 + index * 9;
-
-	$('.runner').css("left", pos+"%");
-    $('.runner').addClass('horizTranslate');
 
 });
 
 $('#nav-left').on('click', function() {
-
+    var MIN_WEEK = 1;
 	var index = $("#hiddenIndex").val();
 
 	index--;
 
-	if(index == 0) {
+	if(index < MIN_WEEK) {
 		$('#nav-left').attr("disabled","disabled");
 	} else {
 		$('#nav-left').removeAttr("disabled");
 		$('#nav-right').removeAttr("disabled");
+        $("#hiddenIndex").val(index);	
+        var pos = 3 + index * 9;
+        $('.runner').css("left", pos+"%");
+        $('.runner').addClass('horizTranslate');
 	}
-
-	$("#hiddenIndex").val(index);	
-
-	var pos = 3 + index * 9;
-
-	$('.runner').css("left", pos+"%");
-    $('.runner').addClass('horizTranslate');
 
 });
