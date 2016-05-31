ef2f0bb7d5218fb8f130547b1488444052290684
haokanga <haokanga@andrew.cmu.edu>
2016-05-31 11:40:30 +0930

    researcher school overview v1.0.0 finished (CRUD class info; alert and tips)

---
+++ isnap2change/researcher/school.php
@@ -0,0 +1,297 @@
+<?php
+    //if true, echo debug output in dev mode, else production mode
+	$DEBUG_MODE = true;    
+	session_start();
+    require_once("../connection.php");	  
+    $conn = db_connect();
+    
+    //set userid    
+    if(isset($_SESSION['researcherid'])){
+        $researcherid = $_SESSION['researcherid'];
+        if($DEBUG_MODE){
+            echo "<script language=\"javascript\">  console.log(\"This is DEBUG_MODE with SESSION ResearcherID = ".$researcherid.".\"); </script>";
+        }
+    }else{
+        if($DEBUG_MODE){
+            echo "<script language=\"javascript\">  console.log(\"This is DEBUG_MODE with hard-code ResearcherID = 1.\"); </script>";
+            $researcherid = 1;
+        }
+    }
+    
+    //if update/insert/remove school
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(isset($_POST['update'])){                          
+            $update = $_POST['update'];
+            //update
+            if($update == 0){
+                $schoolID = $_POST['SchoolID'];;
+                $schoolName = $_POST['SchoolName'];
+                // update class 
+                $update_stmt = "UPDATE School 
+                    SET SchoolName = ?
+                    WHERE SchoolID = ?";			
+                $update_stmt = $conn->prepare($update_stmt);                            
+                if(! $update_stmt -> execute(array($schoolName, $schoolID))){
+                    echo "<script language=\"javascript\">  alert(\"Error occurred to update school. Contact with developers.\"); </script>";
+                } else{
+                }
+            }
+            else if($update == 1){ 
+                $schoolName = $_POST['SchoolName'];
+                // insert school 
+                $update_stmt = "INSERT INTO School(SchoolName)
+                     VALUES (?);";			
+                $update_stmt = $conn->prepare($update_stmt);                
+                if(! $update_stmt -> execute(array($schoolName))){
+                    echo "<script language=\"javascript\">  alert(\"Error occurred to insert school. Contact with developers.\"); </script>";
+                } else{
+                }             
+            }else if($update == -1){
+                $schoolID = $_POST['SchoolID'];;
+                // remove school (with help of DELETE CASCADE) 
+                $update_stmt = "DELETE FROM School WHERE SchoolID = ?";			
+                $update_stmt = $conn->prepare($update_stmt);
+                if(! $update_stmt -> execute(array($schoolID))){
+                    echo "<script language=\"javascript\">  alert(\"Error occurred to delete school. Contact with developers.\"); </script>";
+                } else{
+                } 
+            }            
+        }
+    }
+
+    // get school
+    $schoolSql = "SELECT SchoolID, SchoolName
+               FROM School";
+    $schoolQuery = $conn->prepare($schoolSql);
+    $schoolQuery->execute();
+    $schoolResult = $schoolQuery->fetchAll(PDO::FETCH_OBJ);
+    
+    // get class
+    $classSql = "SELECT ClassID, ClassName, SchoolName
+               FROM Class NATURAL JOIN School";
+    $classQuery = $conn->prepare($classSql);
+    $classQuery->execute();
+    $classResult = $classQuery->fetchAll(PDO::FETCH_OBJ);
+    
+    // get class number
+    $classNumSql = "SELECT count(*) as Count, SchoolID
+               FROM School NATURAL JOIN Class
+               GROUP BY SchoolID";
+    $classNumQuery = $conn->prepare($classNumSql);
+    $classNumQuery->execute();
+    $classNumResult = $classNumQuery->fetchAll(PDO::FETCH_OBJ); 
+    
+    db_close($conn); 
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Content Editor</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="../bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <!-- <link href="../bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet"> -->
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!--w3data.js to include html-->
+    <script src="../js/w3data.js"></script>
+    
+    <style>
+    .glyphicon:hover {
+        background-color: rgb(153, 153, 102);
+    }
+    </style>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <div w3-include-html="navigation.html"></div> 
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">School Overview</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            School Information Table <span class="glyphicon glyphicon-plus pull-right" data-toggle="modal" data-target="#dialog"></span>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="dataTable_wrapper">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>SchoolID</th>
+                                            <th>SchoolName</th>
+                                            <th>Classes</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php for($i=0; $i<count($schoolResult); $i++) {?>
+                                        <tr class="<?php if($i % 2 == 0){echo "odd";} else {echo "even";} ?>">
+                                            <td><?php echo $schoolResult[$i]->SchoolID ?></td>
+                                            <td><?php echo $schoolResult[$i]->SchoolName ?></td>
+                                            <td><?php $count=0; for($j=0; $j<count($classNumResult); $j++){ if ($classNumResult[$j]->SchoolID == $schoolResult[$i]->SchoolID) $count=$classNumResult[$j]->Count; } echo $count; ?><span class="glyphicon glyphicon-remove pull-right" aria-hidden="true"></span><span class="pull-right" aria-hidden="true">&nbsp;</span><span class="glyphicon glyphicon-edit pull-right" data-toggle="modal" data-target="#dialog" aria-hidden="true"></span></td>
+                                        </tr>
+                                    <?php } ?>    
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.table-responsive -->
+                            <div class="well row">
+                                <h4>School Overview Notification</h4>
+                                <div class="alert alert-info">
+                                    <p>Navigate schools by filtering or searching. You can create/update/delete any school.</p>
+                                </div>
+                                <div class="alert alert-danger">
+                                    <p><strong>Reminder</strong> : If you remove one school. All the student data in this school will also get deleted (not recoverable).</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+    <!-- Modal -->
+      <div class="modal fade" id="dialog" role="dialog">
+        <div class="modal-dialog">        
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title" id="dialogTitle">Edit Class</h4>
+            </div>
+            <div class="modal-body">
+            <form id="submission" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <!--if 1, insert; else if 0 update; else if -1 delete;-->
+                <input type=hidden name="update" id="update" value="1"></input>
+                <label for="SchoolID">SchoolID</label>
+                <input type="text" class="form-control dialoginput" id="SchoolID" name="SchoolID">
+                <br><label for="SchoolName">SchoolName</label>
+                <input type="text" class="form-control dialoginput" id="SchoolName" name="SchoolName" required>               
+                <br><label for="Classes">Classes</label>
+                <input type="text" class="form-control dialoginput" id="Classes" name="Classes">
+            </form>
+            </div>
+            <div class="modal-footer">            
+              <button type="button" id="btnSave" class="btn btn-default">Save</button>
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>          
+        </div>
+      </div>
+    <!-- jQuery -->
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- DataTables JavaScript -->
+    <script src="../bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+    <script src="../bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>    
+    
+    <!--jQuery Validate plugin-->
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
+
+    <!-- Page-Level Scripts -->
+    <script>    
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+                responsive: true
+        });            
+    }); 
+    //DO NOT put them in $(document).ready() since the table has multi pages
+    $('.glyphicon-edit').on('click', function (){
+        $('#dialogTitle').text("Edit Class");
+        $('#update').val(0);
+        for(i=0;i<$('.dialoginput').length;i++){                
+            $('.dialoginput').eq(i).val($(this).parent().parent().children('td').eq(i).text());
+        }
+        //disable SchoolID and Classes
+        $('.dialoginput').eq(0).attr('disabled','disabled');
+        $('.dialoginput').eq(2).attr('disabled','disabled');            
+    });
+    $('.glyphicon-plus').on('click', function (){
+        $('#dialogTitle').text("Add Class");
+        $('#update').val(1);
+        for(i=0;i<$('.dialoginput').length;i++){                
+            $('.dialoginput').eq(i).val('');
+        }
+        //disable SchoolID and Classes
+        $('.dialoginput').eq(0).attr('disabled','disabled');
+        $('.dialoginput').eq(2).attr('disabled','disabled');            
+    }); 
+    $('.glyphicon-remove').on('click', function (){
+        if (confirm('[WARNING] Are you sure to remove this school? All the student data in this school will also get deleted (not recoverable).')) {
+            $('#update').val(-1);
+            //fill required input
+            for(i=0;i<$('.dialoginput').length;i++){                
+                $('.dialoginput').eq(i).val($(this).parent().parent().children('td').eq(i).text());
+            }
+            $('#submission').submit();
+        }           
+    });
+    $('#btnSave').on('click', function (){
+        $('#submission').validate();        
+        //enable SchoolID
+        $('.dialoginput').eq(0).prop('disabled',false);
+        $('#submission').submit();
+    });
+    //include html
+    w3IncludeHTML();
+    </script>
+</body>
+
+</html>
