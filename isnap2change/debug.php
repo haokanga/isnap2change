61b6ed908adf8f44c6e65741ab0725de6da98884
haokanga <haokanga@andrew.cmu.edu>
2016-06-06 23:10:35 +0930

    debug_log to debug php variables

--- isnap2change/debug.php
+++ isnap2change/debug.php
@@ -1,4 +1,8 @@
 <?php
     //if true, echo debug output in dev mode, else production mode
 	$DEBUG_MODE = true;
+    
+    function debug_log($message){
+        echo "<script language=\"javascript\">  alert(\"".$message."\"); </script>";
+    }
 ?>
