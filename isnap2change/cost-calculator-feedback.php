b4535cc45b1184c02b2ed4d71e50e811ea9af8a3
wwjsky <weijunw@andrew.cmu.edu>
2016-06-08 19:05:29 +0930

    cost-calculator (unfinished)

---
+++ isnap2change/cost-calculator-feedback.php
@@ -0,0 +1,37 @@
+<?php
+
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		if(isset($_POST['studentid']) && isset($_POST['quizid']) && isset($_POST['answerArr'])){
+			$studentid = $_POST['studentid'];
+			$quizid = $_POST['quizid'];
+			$answerArr = json_decode($_POST['answerArr']);
+		} else {
+			
+		}
+	} else {
+		
+	}
+	
+	$feedback = array();
+	$feedback["detail"] = array();
+	
+	$correctAns = [];
+	
+	$count = 0;
+	
+	for($i = 0; $i < count($answerArr); $i++) {
+		if($ansArr[$i] == $correctAns[$i]) {
+			count++;
+		} else {
+			array_push($feedback["detail"], ($i+1));
+		}
+	}
+	
+	if(count == 3) {
+		$feedback["result"] = "pass"; 
+	} else {
+		$feedback["result"] = "fail";
+	}
+
+	
+?>
