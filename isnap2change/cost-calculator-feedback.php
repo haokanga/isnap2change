f7d27ec2699ce19627f7d07051cbb88e8d6fa7b6
wwjsky <weijunw@andrew.cmu.edu>
2016-06-09 10:08:17 +0930

    connection.php update

--- isnap2change/cost-calculator-feedback.php
+++ isnap2change/cost-calculator-feedback.php
@@ -32,10 +32,35 @@
 	
 	if(count == 3) {
 		$feedback["result"] = "pass";
+		
 		$conn = db_connect();
 		
+		if($conn == null) {
+			$feedback["message"] = "Failed to connect database";
+			exit;
+		}
 		
+		try {
+			$conn->beginTransaction();
+			
+			//UPDATE Quiz_Record	
+			$status = "GRADED";
+			
+			$costCalSubmitSql = "INSERT INTO Quiz_Record(QuizID, StudentID, Status)
+								 VALUES (?,?,?) ON DUPLICATE KEY UPDATE Status = ?;";			
 		
+			$costCalSubmitQuery = $conn->prepare($posterQuizSaveSql);                            
+			$costCalSubmitQuery->execute(array($quizid, $studentid, $status, $status));
+			
+			$conn->commit();
+			
+		} catch(Exception $e) {
+			$conn->rollback();
+			$feedback["message"] = "Failed to update database";
+			exit;
+		}
+		
+		db_close($conn);
 		
 	} else {
 		$feedback["result"] = "fail";
