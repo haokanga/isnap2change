566f09a51cf033e76f9c7abe83c609ab08144e77
haokanga <haokanga@andrew.cmu.edu>
2016-06-11 14:50:10 +0930

    preview and spell check

--- isnap2change/researcher/learning-material-editor.php
+++ isnap2change/researcher/learning-material-editor.php
@@ -34,7 +34,9 @@
 
 <!DOCTYPE html>
 <html>
-<head> 
+<head>
+  <!-- Bootstrap Core CSS -->
+  <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"> 
   <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
   <script>
   tinymce.init({
@@ -42,15 +44,15 @@
   height: 500,
   theme: 'modern',
   plugins: [
-    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    'advlist autolink lists link image charmap preview hr anchor pagebreak',
     'searchreplace wordcount visualblocks visualchars code fullscreen',
     'insertdatetime media nonbreaking save table contextmenu directionality',
     'emoticons template paste textcolor colorpicker textpattern imagetools'
   ],
-  toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-  toolbar2: 'print preview media | forecolor backcolor emoticons',
+  toolbar1: 'insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | emoticons link image media | preview',
   imagetools_toolbar: "rotateleft rotateright | flipv fliph | editimage imageoptions",
   image_advtab: true,
+  browser_spellcheck : true,
   templates: [
     { title: 'Test template 1', content: 'Test 1' },
     { title: 'Test template 2', content: 'Test 2' }
@@ -69,7 +71,7 @@
     <textarea name="richcontenttextarea">
         <?php echo $materialRes->Content; ?>
     </textarea>
-    <input type="submit" name='submitbutton' value="Save" class='submit'/><br>
+    <input type="submit" name='submitbutton' value="Save" class='submit'/> <span class="glyphicon glyphicon-info-sign"></span><b> Ctrl + S</b><br>
 </form>
 </body>
 </html>
