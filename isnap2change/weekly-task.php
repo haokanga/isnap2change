f55255c678f8f2c41be0af40a9dfb8275b51c595
wwjsky <weijunw@andrew.cmu.edu>
2016-06-13 18:06:46 +0930

    weekly-task modified

--- isnap2change/weekly-task.php
+++ isnap2change/weekly-task.php
@@ -42,48 +42,49 @@
 
 	db_close($conn);
 
-    $count = 0;
-	
-    while($quizResult = $quizQuery->fetch(PDO::FETCH_ASSOC)){
-        $count++;
-		
-		if(isset($quizResult["Status"])){
-			//if UNGRADED/GRADED
-			$status = $quizResult["Status"];
-			
+	for($i=0; $i<count($quizzesStatusRes); $i++){
+		if(isset($quizzesStatusRes["Status"])){
+			//if UNSUBMITTED/UNGRADED/GRADED
+			$status = $quizzesStatusRes[$i]->Status;
+
 			//list of question type
-			switch($quizResult["QuizType"]) {
+			switch($quizzesStatusRes[$i]->QuizType){
 				case "MCQ":
-					echo '<form id="quiz'.$quizResult["QuizID"].'" action=multiple-choice-question.php method=post>';
+					echo '<form id="quiz'.$quizzesStatusRes[$i]->QuizID.'" action=multiple-choice-question.php method=post>';
 					break;
 				case "SAQ":
-					echo '<form id="quiz'.$quizResult["QuizID"].'" action=short-answer-question.php method=post>';
+					echo '<form id="quiz'.$quizzesStatusRes[$i]->QuizID.'" action=short-answer-question.php method=post>';
 					break;
 				case "Matching":
-					echo '<form id="quiz'.$quizResult["QuizID"].'" action=matching-question.php method=post>';
+					echo '<form id="quiz'.$quizzesStatusRes[$i]->QuizID.'" action=matching-question.php method=post>';
 					break;
 				case "Poster":
-					echo '<form id="quiz'.$quizResult["QuizID"].'" action=poster-editor.php method=post>';
+					echo '<form id="quiz'.$quizzesStatusRes[$i]->QuizID.'" action=poster-editor.php method=post>';
+					break;
+				case "Calculator":
+					echo '<form id="quiz'.$quizzesStatusRes[$i]->QuizID.'" action=cost-calculator.php method=post>';
 					break;
 				default:
 					break;
 			}
-			
+
 		} else {
 			//if UNANSWERED
 			$status = "UNANSWERED";
-				
-			echo '<form id="quiz'.$quizResult["QuizID"].'" action=learning-material.php method=post>';
+
+			echo '<form id="quiz'.$quizzesStatusRes[$i]->QuizID.'" action=learning-material.php method=post>';
+			echo '<input type=hidden name="status" value='.$status.'>';
 		}
-		
-        echo '<button type=button onclick="startQuiz('.$quizResult["QuizID"].')"> Quiz '.$count.'</button>
-			  <input  type=hidden name="quizID" value='.$quizResult["QuizID"].'>
-			  <input  type=hidden name="quizType" value='.$quizResult["QuizType"].'>
-			  <input  type=hidden name="week" value='.$week.'>
-			  <input type=hidden name="status" value='.$status.'>
+
+		echo '<button type=button onclick="startQuiz('.$quizzesStatusRes[$i]->QuizID.')"> Quiz '.($i+1).'</button>
+			  <input  type=hidden name="quizID" value='.$quizzesStatusRes[$i]->QuizID.'>
+			  <input  type=hidden name="quizType" value='.$quizzesStatusRes[$i]->QuizType.'>
+			  <input  type=hidden name="week" value='.$quizzesStatusRes[$i]->Week.'>
 			  </form>';
-    }
-    //game
+	}
+
+
+    /**  game query move somewhere else
     $gameSql = "SELECT * FROM Game";    
     $gameQuery = $conn->prepare($gameSql);
     $gameQuery->execute(); 
@@ -94,8 +95,7 @@
         <input  type=hidden name="week" value='.$week.'>
         </form>';
     }
-	db_close($conn);	
-	
+	*/
 ?>
 
 <html>
