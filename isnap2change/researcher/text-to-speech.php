9d165e45cd45b3f92e3a0203986d88a8397c8e11
haokanga <haokanga@andrew.cmu.edu>
2016-06-22 11:19:18 +0930

    TODO: Text-To-Speech API exploration

--- isnap2change/researcher/text-to-speech.php
+++ isnap2change/researcher/text-to-speech.php
@@ -5,26 +5,26 @@
 <body>
 
 <script>
-    speak('Dopamine (contracted from 3,4-dihydroxyphenethylamine) is an organic chemical of the catecholamine and phenethylamine families that plays several important roles in the brain and body. It is an amine synthesized by removing a carboxyl group from a molecule of its precursor chemical L-DOPA, which is synthesized in the brain and kidneys. Dopamine is also synthesized in plants and most multicellular animals.');
- 
+    speak('Jon likes Iced Tea!');
+
 // say a message
 function speak(text, callback) {
     var u = new SpeechSynthesisUtterance();
     u.text = text;
-    u.lang = 'en-AU';
- 
+    u.lang = 'en-US';
+
     u.onend = function () {
         if (callback) {
             callback();
         }
     };
- 
+
     u.onerror = function (e) {
         if (callback) {
             callback(e);
         }
     };
- 
+
     speechSynthesis.speak(u);
 }
 </script>
