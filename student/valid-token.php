5504ac2ead693434132f056aaca00f3f96732b74
shivangi <shivangichugh@cmu.edu>
2016-07-01 01:58:58 +0530

    replaced welcome2.php and welcome.php with new welcome.php
    Fixed bugs in signup screen

--- student/valid-token.php
+++ student/valid-token.php
@@ -44,19 +44,22 @@
         </script>
     </head>
     <body>
-            <div class="col-xs-offset-5 col-xs-2" style="height: 100%; text-align: center; align-items: center;">
-                <img class="img-responsive" src="css/image/Snap_Logo_Inverted.png" style="height:28%; margin-top:39%; width: 130%; margin-left: -20%;">
+           
+            <div class="col-md-offset-4 col-md-4 col-xs-10 col-xs-offset-1" style="height: 40%; text-align: center; align-items: center;">
+                <img class="img-responsive" src="css/image/Snap_Logo_Inverted.png" style="height:70%; margin-top:20%; width: 100%; margin-bottom: 8%;">
                 <br>
-                <h2 style="color: white; text-align: center; margin-top:10%;">Verify Token</h2>
+                </div>
+                <div class="col-md-offset-5 col-md-2 col-xs-8 col-xs-offset-2" style="height: 60%; text-align: center; align-items: center; margin-top: 2%;">
+                <span style="color: white; text-align: center; font-size: 4vh;">Verify Token</span>
                 <div style="text-align: center; margin-top: 4%">
                     <span id="token-validation-fail-text" style="color:red"></span>
                 </div>
-                <div class="input-group input-group-lg" style="margin-top:4%; text-align: center; align-items: center;">
+                <div class="input-group input-group-lg" style="margin-top:8%; text-align: center; align-items: center; width: 100%;">
                     <form id="valid-token" action="signup.php" method="post">
-                        <input id="token" name="tokenString" type="password" style="text-align: center; border-radius: 10px; border: none; color:yellow; background-color: black; opacity: 0.7;" class="form-control"  placeholder="TOKEN NUMBER" onfocus="this.placeholder=''" onblur="this.placeholder='Token Number'" aria-describedby="sizing-addon1">
+                        <input id="token" name="tokenString" type="password" style="text-align: center; border-radius: 8px; border: none; color:yellow; background-color: black; opacity: 0.7; font-size: 3.8vh;" class="form-control"  placeholder="Token Number" onfocus="this.placeholder=''" onblur="this.placeholder='Token Number'" aria-describedby="sizing-addon1">
                     </form>
                 </div>
-                <button type="button" onclick="validInfo('VALIDTOKEN')" class="btn btn-primary btn-lg btn-block" style="margin-top:8%; border-radius: 10px; border-color: yellow !important; color:yellow; background-color: black; opacity: 0.7;">Verify</button>
+                <button type="button" onclick="validInfo('VALIDTOKEN')" class="btn btn-primary btn-lg btn-block" style="margin-top:10%; border-radius: 10px; border-color: yellow !important; font-size: 3.6vh; border: yellow solid 4px; color:yellow; background-color: black; opacity: 0.7; width: 80%; margin-left: 10%;">Verify</button>
             </div> 
     </body>
 </html>
