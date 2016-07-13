3383e29cee994a29613c5b77f70ad808ec6f80bb
haokanga <haokanga@andrew.cmu.edu>
2016-07-13 22:31:01 +0930

    Suspended:  Using http basic authentication with Ajax call. Try Java API

--- isnap2change/text-to-speech/tts-ajax.php
+++ isnap2change/text-to-speech/tts-ajax.php
@@ -1,7 +1,12 @@
 /**
 * Created by ABS on 7/13/2016.
 */
-
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Origin, Authorization");
+header("Access-Control-Allow-Credentials: true");
+?>
 <div class="col-lg-6 col-md-6 col-sm-12">
     <audio class="audio">
         Your browser does not support the audio element.
@@ -18,6 +23,50 @@
 <script>
     var voice = 'en-US_AllisonVoice';
     var audio = $('.audio').get(0);
+    var username = "6678c2a8-0261-44f8-a9d3-9a2fb43d65b6";
+    var password = "40byApeYUV7f";
+    function setHeader(xhr) {
+        xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+    }
+
+    $('.speak-button').click(function (evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        $('.result').hide();
+        var text = "How things change in a year. In addition to the header attribute in place of xhr.setRequestHeader, current jQuery (1.7.2+) includes a username and password attribute with the $.ajax call";
+
+        $.ajax
+        ({
+            type: "POST",
+            url: "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize",
+            dataType: 'jsonp',
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            headers: {
+                Accept: "audio/wav",
+                "Content-Type": "Content-Type: application/json"
+            },
+            data: "{\"text\":\"Eating a balanced diet is vital.\"}",
+            success: function () {
+                alert('Success!');
+            },
+            error: function () {
+                alert('Failed!');
+            },
+            beforeSend: setHeader
+        });
+        //synthesizeRequest(utteranceOptions, audio);
+
+        /*
+         * curl -X POST -u 6678c2a8-0261-44f8-a9d3-9a2fb43d65b6:40byApeYUV7f --header "Content-Type: application/json" --header "Accept: audio/wav" --data "{\"text\":\"Eating a balanced diet is vital for your health and wellbeing. The food we eat is responsible for providing us with the energy to do all the tasks of daily life. For optimum performance and growth a balance of protein, essential fats, vitamins and minerals are required. We need a wide variety of different foods to provide the right amounts of nutrients for good health. The different types of food and how much of it you should be aiming to eat is demonstrated on the pyramid below.\"}" --output balanced_diet.wav "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize"
+         *
+         * */
+
+        return false;
+    });
+
 
     function synthesizeRequest(options, audio) {
         var sessionPermissions = JSON.parse(localStorage.getItem('sessionPermissions')) ? 0 : 1;
@@ -39,21 +88,4 @@
         $('.speak-button').css('cursor', 'wait');
         return true;
     }
-
-    $('.speak-button').click(function (evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-        $('.result').hide();
-        var text = "How things change in a year. In addition to the header attribute in place of xhr.setRequestHeader, current jQuery (1.7.2+) includes a username and password attribute with the $.ajax call";
-
-        var utteranceOptions = {
-            text: text,
-            voice: voice,
-            sessionPermissions: JSON.parse(localStorage.getItem('sessionPermissions')) ? 0 : 1
-        };
-
-        synthesizeRequest(utteranceOptions, audio);
-
-        return false;
-    });
 </script>
