2bf99d6c06cadfb1fd5b9e8de2acb4fbfcb76003
haokanga <haokanga@andrew.cmu.edu>
2016-07-15 02:15:13 +0930

    [Finished] tuned tts: remove whitespace and child text

--- researcher/researcher-tts.js
+++ researcher/researcher-tts.js
@@ -7,9 +7,14 @@ function appendTTSButton() {
     $('.dataTable td').each(function () {
         // ignore hidden cols
         if ($(this).is(':visible')) {
-            var text = $(this).text();
+            var text = $(this).clone() //clone the element
+                .children() //select all the children
+                .remove() //remove all the children
+                .end() //again go back to selected element
+                .text()
+                .trim();
             // ignore no-text/numeric cells
-            if (text.trim().length > 0 && !$.isNumeric(text.trim())) {
+            if (text.length > 0 && !$.isNumeric(text)) {
                 $(this).append($('<span class="glyphicon glyphicon-volume-up pull-right tts" aria-hidden="true"></span>'));
             }
         }
--- student/js/tts.js
+++ student/js/tts.js
@@ -1,18 +1,25 @@
 //text and icon span shares the same parent
 $(document).ready(function () {
-    $('.tts').on('click', function (){
+    $('.tts').on('click', function () {
         //encode text for http request parameters
-        var text = encodeURIComponent($(this).parent().text());
+        var text = encodeURIComponent($(this).parent()
+            .clone() //clone the element
+            .children() //select all the children
+            .remove() //remove all the children
+            .end() //again go back to selected element
+            .text()
+            .trim()
+        );
 
         //remove old tts if exists
-        $(this).children().filter("audio").each(function(){
+        $(this).children().filter("audio").each(function () {
             this.pause();
             $(this).remove();
         });
         $(this).empty();
 
         //append new tts with en-US_AllisonVoice
-        var ttsAudio =  $( '<audio autoplay hidden><source src="https://text-to-speech-demo.mybluemix.net/api/synthesize?voice=en-US_AllisonVoice&text='+ text +'&X-WDC-PL-OPT-OUT=1" type="audio/ogg"></audio>' );
+        var ttsAudio = $('<audio autoplay hidden><source src="https://text-to-speech-demo.mybluemix.net/api/synthesize?voice=en-US_AllisonVoice&text=' + text + '&X-WDC-PL-OPT-OUT=1" type="audio/ogg"></audio>');
         $(this).append(ttsAudio);
     });
 });
