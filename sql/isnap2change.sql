c1856c866135e6cb83e5009e7cf96b0799a8c226
haokanga <haokanga@andrew.cmu.edu>
2016-07-17 19:26:08 +0930

    [Finished] debug MySQL logger TODO: unit test

--- sql/isnap2change.sql
+++ sql/isnap2change.sql
@@ -1,5 +1,23 @@
 #mysql -uroot -p.kHdGCD2Un%P
 #mysql -uroot -p.kHdGCD2Un%P < isnap2change.sql
+
+CREATE DATABASE IF NOT EXISTS log;
+USE log;
+CREATE TABLE IF NOT EXISTS `Log` (
+    LogID MEDIUMINT AUTO_INCREMENT,
+    PageName TEXT,
+    RequestMethod TEXT,
+    RequestParameters TEXT,
+    SessionDump TEXT,
+    ExceptionMessage LONGTEXT,
+    ExceptionTrace LONGTEXT,
+    UserFeedback TEXT,
+    Solved BOOLEAN DEFAULT 0,
+    LogTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT Log_LogID_PK PRIMARY KEY (LogID)
+)  ENGINE=INNODB;
+
+
 CREATE DATABASE IF NOT EXISTS isnap2changedb;
 USE isnap2changedb;
 
